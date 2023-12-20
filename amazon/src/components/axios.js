585d32c8f5eb30ef58e7d12ee335de2bd1ac4c51
MAbuhanif <mundinoabete@gmail.com>
2023-12-20 21:05:02 +0000

    new url added

--- amazon/src/components/axios.js
+++ amazon/src/components/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:10000",
+  // baseURL: "http://localhost:10000",
+  baseURL: "https://amazon-by-mundino.cyclic.app/",
   // baseURL: "http://127.0.0.1:5001/clone-54cab/us-central1/api",
 });
 export default instance;
